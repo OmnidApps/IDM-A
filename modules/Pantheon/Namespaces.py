9b8be0061cb8253e4e82e812cba43497e4e92bc3
Josh Santos <omnidapps@protonmail.com>
2022-05-29 21:30:53 +0000

    Fix pylint errors

--- modules/Pantheon/Namespaces.py
+++ modules/Pantheon/Namespaces.py
@@ -2,24 +2,17 @@ from flask_restx import Namespace
 
 
 class NamespaceWrapper(Namespace):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def expect_header(self, name, desc):
         return self.doc(params={name: {"in": "header", "description": desc}})
 
     def input_schema(self, schema):
-        # user_api needs abstracted away here so that this can be reusable in a shared library for other namespaces
-        return self.expect( schema )
+        return self.expect(schema)
 
     def expect_url_var(self, variable, desc):
-        # user_api needs abstracted away here so that this can be reusable in a shared library for other namespaces
-        return self.param( variable, desc )
+        return self.param(variable, desc)
 
     def output_schema(self, schema):
-        # user_api needs abstracted away here so that this can be reusable in a shared library for other namespaces
-        return self.marshal_list_with( schema, mask='' )
-
+        return self.marshal_list_with(schema, mask='')
 
 
 user_api = NamespaceWrapper('user', description='User Management API')
